868dc0fc5e5e22facc807c22bfaba9eb29f12f4a
Dipankar Sarkar <dipankarsarkar@gmail.com>
2014-12-13 16:15:59 +0530

    Pushing dummy data into riemann

--- main.go
+++ main.go
@@ -7,18 +7,18 @@ import (
 
 	"github.com/bigdatadev/goryman"
 	"github.com/golang/glog"
-	"github.com/google/cadvisor/info"
 	"github.com/google/cadvisor/client"
+	"github.com/google/cadvisor/info"
 )
 
 var reimannAddress = flag.String("reimann_address", "localhost:5555", "specify the reimann server location")
 var cadvisorAddress = flag.String("cadvisor_address", "http://localhost:8080", "specify the cadvisor API server location")
-var sampleInterval = flag.Int("sample_interval",1000,"specify the sampling interval")
+var sampleInterval = flag.Int("sample_interval", 1000, "specify the sampling interval")
 
 func main() {
 	defer glog.Flush()
 	flag.Parse()
-	
+
 	// Setting up the Reimann client
 	r := goryman.NewGorymanClient(*reimannAddress)
 	err := r.Connect()
@@ -32,8 +32,8 @@ func main() {
 	if err != nil {
 		glog.Fatalf("unable to setup cadvisor client: %s", err)
 	}
-	
-	// Setting up the 1 second ticker 
+
+	// Setting up the 1 second ticker
 	ticker := time.NewTicker(1 * time.Second).C
 	for {
 		select {
@@ -48,8 +48,18 @@ func main() {
 			// Loop into each ContainerInfo
 			// Get stats
 			// Push into reimann
-			fmt.Println(returned[0].Stats[0])
+			for _, container := range returned {
+				fmt.Println(container.Stats[0])
+				err = r.SendEvent(&goryman.Event{
+					Service: "moargore",
+					Metric:  100,
+					Tags:    []string{"nonblocking"},
+				})
+				if err != nil {
+					glog.Fatalf("unable to write to riemann: %s", err)
+				}
+			}
 		}
 	}
-	
+
 }
